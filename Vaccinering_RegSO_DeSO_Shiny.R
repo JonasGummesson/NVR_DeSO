f4b70dd8fda636633ac4fc11bce11d2bb3977edc
JonasGummesson <jonas.gummesson@gmail.com>
2021-10-18 13:36:44 +0200

    Update Vaccinering_RegSO_DeSO_Shiny.R

--- Vaccinering_RegSO_DeSO_Shiny.R
+++ Vaccinering_RegSO_DeSO_Shiny.R
@@ -98,6 +98,21 @@ sf_result_regso <-
 
 
 
+sf_result_regso  %>%
+  filter(KOMMUNNAMN == "Falun") %>%
+  filter(Period == as.POSIXct("2021-09-29")) %>%
+  filter(Doser %in% "Minst 1 dos") %>%
+  filter(Ålder %in% "18-64") %>%
+  mutate(Intervall.hög.min = min(Intervall.hög),
+         Intervall.hög.max = max(Intervall.hög)) %>%
+  ggplot() + 
+  geom_sf(aes(fill = Intervall))+  
+  geom_label_repel(aes(label = RegSO,  x = RegSO_x, y = RegSO_y), force=50, size=4, max.overlaps = 1000, color = "black") +
+  scale_fill_manual(values = viridis::magma(n = 9))+
+  theme_minimal()+
+  theme(axis.title.x=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank())+
+  theme(axis.title.y=element_blank(),axis.text.y=element_blank(),axis.ticks.y=element_blank())+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
 
 
